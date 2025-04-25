4dd6de65d5ddf22a44ebcb177019451845a2733c
Emartorell07 <eduardomartorell38@gmail.com>
2025-04-25 15:51:58 -0600

    Subida de codigo

---
+++ Laboratorios_Progra_I/src/laboratorios_progra_i/Laboratorios_Progra_I.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package laboratorios_progra_i;
+
+/**
+ *
+ * @author eduar
+ */
+public class Laboratorios_Progra_I {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
